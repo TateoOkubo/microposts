1e27eacc60fb183bb210746dfc836772d0aa58ea
Tateo Okubo <neteooo@gmail.com>
2016-12-25 18:32:41 +0000

    bug fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,13 +3,16 @@ class StaticPagesController < ApplicationController
   # ログインしている場合は新しいMicropost食らうｓのインスタンスをuser_idに紐づけた状態で初期化
   
   def home
-    # current_user.microposts.build　は 
-    # Micropost.new(user_id: current_user.id) と同じ
-    # 前者の方を使うようにする
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
     
-    # feed_itemsで現在のユーザのフォローしているユーザのマイクロポストを取得，
-    # 作成日時が新しいもの順で並び替える
-    @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+      # current_user.microposts.build　は 
+      # Micropost.new(user_id: current_user.id) と同じ
+      # 前者の方を使うようにする
+      @micropost = current_user.microposts.build if logged_in?
+    
+      # feed_itemsで現在のユーザのフォローしているユーザのマイクロポストを取得，
+      # 作成日時が新しいもの順で並び替える
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+    end
   end
 end
